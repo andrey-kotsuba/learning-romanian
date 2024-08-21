52c6e310c5cd0746c9005d5f78bcbedcb9d1b394
kabucya <kabucya@alterpost.org>
2024-08-22 00:11:25 +0300

    add PWA

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,22 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/learning-romanian/' : '/',
+  pwa: {
+    name: 'Learning Romanian', // Название вашего приложения
+    themeColor: '#ba4d4d', // Цвет темы
+    msTileColor: '#000000', // Цвет плитки для Windows
+    appleMobileWebAppCapable: 'yes', // Разрешение для мобильных устройств Apple
+    appleMobileWebAppStatusBarStyle: 'black', // Стиль строки состояния для мобильных устройств Apple
+
+    // настройки манифеста PWA
+    manifestOptions: {
+      background_color: '#ba4d4d'
+    },
+
+    // Настройки Service Worker
+    workboxOptions: {
+      skipWaiting: true, // Принудительное обновление Service Worker
+      clientsClaim: true, // Захват всех клиентов сразу после активации
+    }
+  }
 })
